65e185b9155a5ea690e49a3273e8704f7dc56c54
Ruben Biesemans <ruben.biesemans@spikes.be>
2017-07-14 11:09:30 +0200

    Finished v0.0.1 and demo app

--- demo/SpikesNgComponentsDemo/src/app/spikes-treeview-demo/spikes-treeview-demo.component.ts
+++ demo/SpikesNgComponentsDemo/src/app/spikes-treeview-demo/spikes-treeview-demo.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
+import * as tree from 'spikes-ng2-components';
+
 @Component({
   selector: 'app-spikes-treeview-demo',
   templateUrl: './spikes-treeview-demo.component.html',
@@ -7,9 +9,45 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SpikesTreeviewDemoComponent implements OnInit {
 
+  treeviewItems: Array<tree.ITreeviewItem> = [];
+  treeviewConfiguration: tree.ITreeviewConfiguration = new tree.TreeviewConfiguration({
+    showToggleAll: true
+  });
+
   constructor() { }
 
   ngOnInit() {
+    this.initTreeview();
+  }
+
+  private initTreeview():void{
+    let items: Array<tree.ITreeviewItem> = [];
+
+    items = [...this.getMockTreeviewItems("TopLevel", 5)]
+    items.forEach(item => {
+      item.childItems = [...this.getMockTreeviewItems("DirectChild", 4)];
+      item.childItems.forEach(i => {
+        i.childItems = [...this.getMockTreeviewItems("LowestLevel", 3)];
+      });
+    });
+
+    this.treeviewItems = [...items];
+  }
+
+  private getMockTreeviewItems(text: string, itemCount: number): Array<tree.ITreeviewItem>{
+    let items: Array<tree.ITreeviewItem> = [];
+
+    for(let i: number = 0; i < itemCount; i++){
+      items.push(
+        new tree.TreeviewItem ({
+          id: i+1,
+          displayText: `${text}-${(i+1).toString()}`,
+          itemState: tree.CheckState.Unchecked
+        })
+      );
+    }
+
+    return items;
   }
 
 }
--- src/gulpfile.ts
+++ src/gulpfile.ts
@@ -1,24 +1,25 @@
 
 var gulp = require('gulp'),
   path = require('path'),
-  sass    = require('gulp-sass'),
+  sass = require('gulp-sass'),
   runSequence = require('run-sequence'),
-  minifyCss   = require('gulp-minify-css'),
+  minifyCss = require('gulp-minify-css'),
   sourcemaps = require('gulp-sourcemaps'),
-  tsc  = require('gulp-typescript'),
-  embedTemplates  = require('gulp-inline-ng2-template'),
+  tsc = require('gulp-typescript'),
+  embedTemplates = require('gulp-inline-ng2-template'),
   del = require('del'),
   exec = require('child_process').exec;
 
 const rootFolder = path.join(__dirname);
 const distFolder = path.join('../dist');
+const tmpFolder = './tmp';
 
 //----
 //build steps
 gulp.task('build', function (done) {  
   runSequence(
-    'clean',
-    'compile-sass',
+    'clean-dist',
+    'pre-compile',
     'compile-typings',
     'copy-required',
     'clean-src'
@@ -27,29 +28,30 @@ gulp.task('build', function (done) {
 
 //----
 //clearing the output dir
-gulp.task('clean', function (done) {
+gulp.task('clean-dist', function (done) {
   // Delete contents but not dist folder to avoid broken npm links
   // when dist directory is removed while npm link references it.
   return deleteFolders([distFolder + '/**', '!' + distFolder]);
 });
 
 //----
+//Test inlining html
+gulp.task('pre-compile', function(){
+  //loading typings file
+    var tsProject = tsc.createProject('tsconfig.json');
+
+    return  tsProject.src()
+        .pipe(embedTemplates({ 
+            base:'./',
+            useRelativePaths: true 
+        }))        
+        .pipe(gulp.dest(tmpFolder));
+});
+
+//----
 //typescript compilation including sourcemaps and template embedding
-gulp.task('compile-typings', function(cb) {
-
-    //loading typings file
-    // var tsProject = tsc.createProject('tsconfig.json');
-
-    // return  tsProject.src()
-    //     .pipe(embedTemplates({ 
-    //         base:'./',
-    //         useRelativePaths: true 
-    //     }))
-    //     .pipe(tsProject())
-    //     .pipe(sourcemaps.init())
-    //     .pipe(sourcemaps.write('./'))
-    //     .pipe(gulp.dest(distFolder));
-    exec('"./node_modules/.bin/ngc" -p "tsconfig.json"', function(err, stdout, stderr){
+gulp.task('compile-typings', function(cb) {    
+    exec('"./node_modules/.bin/ngc" -p "ngcconfig.json"', function(err, stdout, stderr){
       console.log(stdout);
       console.log(stderr);
       cb(err);
@@ -62,14 +64,6 @@ gulp.task('copy-required', function () {
      gulp
       .src(['package.json', 'LICENCE'])
       .pipe(gulp.dest(distFolder));
-});
-
-//----
-//Copy html
-gulp.task('copy-html', function () {
-     gulp
-      .src(['components/**/*.html'])
-      .pipe(gulp.dest(path.join(distFolder, 'components')));
 });
 
 //----
@@ -89,8 +83,10 @@ gulp.task('clean-src', function(){
   return del([
     '**/*.ngsummary.json',
     '**/*.ngfactory.ts',
-    '**/*.shim.ngstyle.ts'
-  ])
+    '**/*.shim.ngstyle.ts',
+    '!tmp',
+    'tmp/**/*',
+  ]);
 });
 
 /**
