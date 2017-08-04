31d2572b2756b8f33ef76f9654d08d37da0fed9a
Ruben Biesemans <ruben.biesemans@spikes.be>
2017-08-04 15:51:03 +0200

    Test new items in timeline

--- demo/SpikesNgComponentsDemo/src/app/spikes-timeline-demo/spikes-timeline-demo.component.ts
+++ demo/SpikesNgComponentsDemo/src/app/spikes-timeline-demo/spikes-timeline-demo.component.ts
@@ -14,19 +14,26 @@ export class SpikesTimelineDemoComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
-    for (let i: number = 0; i < 5; i++){
-      this.timelineItems.push(new tl.TimelineItem({
-        id: i,
-        displayText: `Item ${i.toString()}`,
-        color: i < 3 ? 'primary' : i === 3 ? 'test' : 'grey',
-        isActive: i === 0 ? true : false
-      }));
-    }
+    this.timelineItems = [...this.createTimelineItems(5)];
   }
 
   private onTimelineItemAction(item: tl.ITimelineEventArgs){
     console.log('TimelineItem Clicked');
     console.log(item);
+    this.timelineItems = [...this.createTimelineItems(6)]
+  }
+
+  private createTimelineItems(maxItems: number): Array<tl.ITimelineItem>{
+    let items: Array<tl.ITimelineItem> = [];
+    for (let i: number = 0; i < maxItems; i++){
+      items.push(new tl.TimelineItem({
+        id: i,
+        displayText: `Item ${i.toString()}`,
+        color: i < 2 ? 'primary' : i < 3 ? 'secondary' : i === 3 || i === 5 ? 'test' : 'grey',
+        isActive: i === 0 ? true : false
+      }));
+    }
+    return items;
   }
 
 }
